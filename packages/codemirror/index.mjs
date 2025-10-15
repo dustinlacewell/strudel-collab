3a8ae81bce7bdd27c251af290b3ec9d5e5af58d9
Dustin Lacewell <dlacewell@gmail.com>
2025-10-15 04:56:45 -0500

    Fix missing exports

--- packages/codemirror/index.mjs
+++ packages/codemirror/index.mjs
@@ -2,6 +2,8 @@ export * from './codemirror.mjs';
 export * from './autocomplete.mjs';
 export * from './tooltip.mjs';
 export * from './flash.mjs';
+export * from './highlight.mjs';
+export * from './slider.mjs';
 export * from './themes.mjs';
 export * from './collab.mjs';
 export * from './widget.mjs';
